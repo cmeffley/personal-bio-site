09834369c655721a4ddb64278ab9602095e1f73c
Chris Meffley <cmeffley@gmail.com>
2021-05-14 21:04:57 -0400

    started piecing pic together

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import front from '../assets/front.png';
+import side from '../assets/side.png';
+import mountain from '../assets/mountain.png';
+import sky from '../assets/sky.png';
 
 function Home() {
   return (
     <>
-      <h1>Welcome to home</h1>
+      <header className='header'>
+        <img src={front} className='front' />
+        <img src={side} className='side'/>
+        <img src={mountain} className='mountain'/>
+        <img src={sky} className='sky'/>
+      </header>
     </>
   );
 }
