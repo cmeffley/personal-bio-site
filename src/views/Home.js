8f0586c1621ee63315f95a8faa449cbd9c3d8c2d
Chris Meffley <cmeffley@gmail.com>
2021-05-19 17:41:36 -0400

    working parallax

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import front from '../assets/front.png';
 import side from '../assets/side.png';
 import mountain from '../assets/mountain.png';
 // import sky from '../assets/sky.png';
+import About from './About';
+import Tech from './TechUsed';
 
 function Home() {
+  const [offSetY, setOffSetY] = useState(0);
+  const handleScroll = () => {
+    setOffSetY(window.pageYOffset);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
     <>
-      <h1 className='greeting'>Hello</h1>
-      <h1 className='name'>I&apos;m Chris Meffley</h1>
+      <h1 className='greeting'
+        style={{ transform: `-translateY(${offSetY * 0.8}px)` }}
+      >Hello</h1>
+      <h1 className='name'
+        style={{ transform: `-translateY(${offSetY * 0.8}px)` }}
+      >I&apos;m Chris Meffley</h1>
       <header className='header'>
-        <img src={front} className='front' />
-        <img src={side} className='side'/>
-        <img src={mountain} className='mountain'/>
+        <img src={front} className='front'
+          style={{ transform: `translateY(${offSetY * 0.4}px)` }}
+        />
+        <img src={side} className='side'
+          style={{ transform: `translateY(${offSetY * 0.4}px)` }}
+        />
+        <img src={mountain} className='mountain'
+          style={{ transform: `translateY(${offSetY * 0.3}px)` }}
+        />
         {/* <img src={sky} className='sky'/> */}
       </header>
+      <About style={{ transform: `translateY(${offSetY * 0.1}px)` }}/>
+      <Tech />
     </>
   );
 }
