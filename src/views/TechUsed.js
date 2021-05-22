72c47024d66151690c686fc9c5648e2f14f003ab
Chris Meffley <cmeffley@gmail.com>
2021-05-22 12:08:22 -0400

    tech icons showing

--- src/views/TechUsed.js
+++ src/views/TechUsed.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getTechUsed } from '../helpers/data/ProjectData';
+import Tech from '../components/Tech';
 
 function TechUsed() {
+  const [tech, setTech] = useState([]);
+  useEffect(() => {
+    getTechUsed().then((response) => setTech(response));
+  }, []);
   return (
-    <div>
-      <h1>Tech Used</h1>
+    <div id='tech-container'>
+      {tech.map((techInfo) => (
+        <Tech
+        key={techInfo.firebaseKey}
+        {...techInfo}
+        setTech={setTech}
+        />
+      ))}
     </div>
   );
 }
