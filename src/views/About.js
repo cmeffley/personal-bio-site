6d5b1805f8470fce47fc91548e73157cf713719f
Chris Meffley <cmeffley@gmail.com>
2021-07-10 16:36:19 -0400

    changed favicon and project card size

--- src/views/About.js
+++ src/views/About.js
@@ -10,7 +10,7 @@ function About() {
       <div className='aboutMe'>
         <h2>I am a Full Stack Web Developer</h2><br/>
           <p>I have been a zookeeper for almost a decade, and most recently a veterinary technician. During the pandemic, I returned to zookeeping but really starting thinking about a new career that would continue to challenge me for years to come.</p>
-          <p>So how did I get from animal care to Nashville Software School? I have always been intrigued by how things work, and there are several similarties between web development and animal care. You have to be adaptable, a problem solver, think outside the box and a valuable team member.</p>
+          <p>So how did I get from animal care to Nashville Software School? I have always been intrigued by how things work, and there are several similarities between web development and animal care. You have to be adaptable, a problem solver, think outside the box and a valuable team member.</p>
           <p>I am an avid hiker, Ironman finisher and Eagle Scout. I enjoy learning and trying new things, and I hope to find my version of success in the web development community. </p>
         <Button
           inverted
